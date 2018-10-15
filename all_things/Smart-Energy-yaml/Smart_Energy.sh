7954375ae9249ac766c69d32f5a8547966cb5855
seunghyungLee <lshyeung@smartx.kr>
2018-10-15 15:28:22 +0000

    commit

--- all_things/Smart-Energy-yaml/Smart_Energy.sh
+++ all_things/Smart-Energy-yaml/Smart_Energy.sh
@@ -9,7 +9,7 @@ kubectl apply -f zookeeper.yaml
 kubectl apply -f kafka.yaml
 kubectl apply -f influx_chro.yaml
 kubectl apply -f api_server.yaml
-kubectl apply -f consumer_device1.yaml
+kubectl apply -f consumer_d1_inference.yaml
 
 
 
